121d369288678aed15af37ab03ab1b5193ac63bd
Joseph Alfredo <jalfredopoly@gmail.com>
2018-01-28 21:29:52 -0800

    declare engine at package

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
+// heroku addons:create mongolab:sandbox
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp',{
 	// remove warning
 	useMongoClient: true
