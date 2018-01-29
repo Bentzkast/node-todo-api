14e713d96efffa38e42fdf856478338108df2800
Joseph Alfredo <jalfredopoly@gmail.com>
2018-01-28 21:17:20 -0800

    fix heroku

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -5,7 +5,7 @@ mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp',
 	// remove warning
 	useMongoClient: true
 })
-.then(()=>{} )
+.then(()=>{console.log('connected to',process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp')} )
 .catch(err => console.log(err));
 
 module.exports.mongoose = mongoose;
